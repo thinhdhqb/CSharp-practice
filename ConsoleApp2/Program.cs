a16425c70dec17ef3b7ca37f01a3ebb7101be504
Quang Thinh <thinhdhqb@gmail.com>
2022-08-24 17:24:02 +0700

    Revert push

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -27,27 +27,5 @@ namespace ConsoleApp2
             Console.WriteLine("MAX: " + max);
             Console.WriteLine("MIN: " + min);
         }
-
-        static void Frequency()
-        {
-            Console.WriteLine("============");
-            Console.Write("Nhap n: ");
-            int n = Convert.ToInt32(Console.ReadLine());
-            List<int> list = new List<int>(n);
-            Dictionary<int, int> dict = new Dictionary<int, int>();
-            int val;
-            for (int i = 0; i < n; i++)
-            {
-                Console.Write("Nhap phan tu " + i + ": ");
-                val = Convert.ToInt32(Console.ReadLine());
-                if (dict.ContainsKey(val)) dict[val]++;
-                else dict[val] = 1;
-                list.Add(val);
-            }
-            foreach(var item in dict)
-            {
-                Console.WriteLine(item.Key + ": " + item.Value + " times.");
-            }
-        }
     }
 }
