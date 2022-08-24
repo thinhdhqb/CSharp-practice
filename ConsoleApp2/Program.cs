bc4dd3947ac63ad21d78a070bbb6b51259192ca4
Quang Thinh <thinhdhqb@gmail.com>
2022-08-24 17:28:33 +0700

    Add solution to fiding frequency of list elements.

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,22 +10,50 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            List<int> list = new List<int>();
+            //Min_max();
+            Frequency();
+            Console.ReadLine();
+        }
+        
+        static void Min_max()
+        {
             Console.Write("Nhap n: ");
             int n = Convert.ToInt32(Console.ReadLine());
+            List<int> list = new List<int>(n);
             for (int i = 0; i < n; i++)
             {
                 Console.Write("Nhap phan tu " + i + ": ");
                 list.Add(Convert.ToInt32(Console.ReadLine()));
             }
             int max = list[0], min = list[0];
-            foreach(int x in list)
+            foreach (int x in list)
             {
                 if (x > max) max = x;
                 if (x < min) min = x;
             }
             Console.WriteLine("MAX: " + max);
             Console.WriteLine("MIN: " + min);
+        }
+
+        static void Frequency()
+        {
+            Console.WriteLine("==============");
+            Console.Write("Nhap n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            List<int> list = new List<int>(n);
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Nhap phan tu " + i + ": ");
+                int val = Convert.ToInt32(Console.ReadLine());
+                if (dict.ContainsKey(val)) dict[val]++;
+                else dict[val] = 1;
+                list.Add(val);
+            }
+            foreach(var item in dict)
+            {
+                Console.WriteLine(item.Key + ": " + item.Value + " times.");
+            }
         }
     }
 }
